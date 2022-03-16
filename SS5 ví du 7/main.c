a55ed24a429cb5d459f14b1f355e8ed593207816
mxt629 <xuantien.06021990@gmail.com>
2022-03-16 11:53:44 +0700

    Initial Commit

---
+++ SS5 ví du 7/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  SS5 ví du 7
+//
+//  Created by Little Chick on 16/03/2022.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
